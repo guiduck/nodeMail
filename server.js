ee3e77e1eef27e89749280ba033ca6ae6ad172ef
Guilherme <guiduck02@gmail.com>
2020-02-11 22:43:30 -0300

    made nodemailer send an email. next will send img att

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
+require("dotenv").config();
+
 const nodemailer = require("nodemailer");
+
+let transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD
+  }
+});
+
+let mailOptions = {
+  from: "guiduck02@gmail.com",
+  to: "gussstavo95@gmail.com",
+  subject: "nnodemailer test",
+  text: "fuck me in the ass daddy please"
+};
+
+transporter.sendMail(mailOptions, function(err, data) {
+  if (err) {
+    console.log("error occurs:", err);
+  } else {
+    console.log("Email sent!");
+  }
+});
