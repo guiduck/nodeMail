32d6f59525996d501c5b536c3565b7471f43604a
Guilherme <guiduck02@gmail.com>
2020-02-13 00:14:13 -0300

    created html form sending data to server

--- server.js
+++ server.js
@@ -1,6 +1,34 @@
 require('dotenv').config();
 
 const nodemailer = require('nodemailer');
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3333;
+
+//get data from html
+app.use(
+  express.urlencoded({
+    extended: false
+  })
+);
+
+app.use(express.json());
+
+app.post('/email', (req, res) => {
+  //will send mail here
+  console.log('Data', req.body);
+  res.json({ message: 'email route response message boy' });
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log('server starting on port, ', 3333);
+});
 
 let transporter = nodemailer.createTransport({
   service: 'gmail',
